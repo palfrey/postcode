c4a44614cc0c3f974a9c3e9922aac1f0e28082f2
Tom Parker <palfrey@tevp.net>
2010-04-23 18:45:52 +0100

    Use json data from ukpostcodes.com

--- src/net/tevp/Postcode/Postcode.java
+++ src/net/tevp/Postcode/Postcode.java
@@ -5,6 +5,8 @@ import java.net.URL;
 import java.io.IOException;
 import java.io.InputStream;
 
+import org.json.*;
+
 import android.app.Activity;
 import android.os.Bundle;
 import android.location.*;
@@ -34,25 +36,40 @@ public class Postcode extends Activity implements LocationListener {
     }
 
     static Geohash e = new Geohash();
-
+    
 	static String get(double lat, double lon)
 	{
 		String s = e.encode(lat, lon);
 		URL what;
-		try {
-			what = new URL("http://whatismypostcode.appspot.com/"+s);
+		try {			
+			what = new URL(String.format("http://www.uk-postcodes.com/latlng/%.8f,%.8f.json",lat,lon));
 		} catch (MalformedURLException e1) {
 			return "malformed for"+s;
 		}
 		InputStream data;
+		int max = 1000, sofar=0;
+		
+		byte[] out = new byte[max];
 		try {
 			data = what.openStream();
 
-			byte[] out = new byte[10];
-			data.read(out,0,10);
-			return new String(out);//+s+" - "+Double.toString(lat)+", "+Double.toString(lon);
+			while (sofar<max)
+			{
+				int ret = data.read(out,sofar,max-sofar);
+				if (ret == -1)
+					break;
+				sofar += ret;
+			}
 		} catch (IOException e1) {
 			return "ioexception "+e1.getMessage();
+		} 
+		try 
+		{
+			JSONObject js = new JSONObject(new String(out));
+			return js.getString("postcode");
+		}
+		catch (JSONException e2) {
+			return "jsonexception "+e2.getMessage()+"\n"+new String(out);
 		}
 	}
 
