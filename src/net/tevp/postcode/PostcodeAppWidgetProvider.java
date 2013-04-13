ebcf4aaea796ebf40d2cbd99d67a905c251d5671
Andrew Dunn <dunnad@gmail.com>
2013-04-13 22:54:30 +0100

    Removed service and use PostcodeBackend.getPostCode, added time and lat/long to widget.

--- src/net/tevp/postcode/PostcodeAppWidgetProvider.java
+++ src/net/tevp/postcode/PostcodeAppWidgetProvider.java
@@ -1,112 +1,57 @@
 package net.tevp.postcode;
 
-import android.app.Service;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.ComponentName;
 import android.content.Context;
-import android.content.Intent;
-import android.location.Criteria;
 import android.location.Location;
-import android.location.LocationListener;
-import android.location.LocationManager;
-import android.os.Bundle;
-import android.os.IBinder;
-import android.util.Log;
 import android.widget.RemoteViews;
 
-/**
- *
- */
-public class PostcodeAppWidgetProvider extends AppWidgetProvider {
-    private final static String TAG = PostcodeAppWidgetProvider.class.getName();
+import java.text.DateFormat;
 
-    private final static long FIVE_MINS_MS = 5 * 60 * 1000;
-    private final static int FIVE_HUNDRED_M = 500;
+/**
+ * TODO: Add in the pending intents to handle, click to start the main app, click on accuracy to refresh
+ * TODO: Handle size changes to show extras  - and persist post code  / location
+ */
+public class PostcodeAppWidgetProvider extends AppWidgetProvider implements PostcodeListener {
+
+    private final PostcodeBackend pb = new PostcodeBackend();
+    private Location lastLocation = null;
+    private Context context;
 
     @Override
     public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
-        Log.d("PostcodeAppWidgetProvider","onUpdate");
-        context.startService(new Intent(context, UpdateService.class));
+        pb.getPostcode(context, this, true);
+        this.context = context;
     }
 
-    public static class UpdateService extends Service implements LocationListener {
+    @Override
+    public void postcodeChange(final String postcode) {
+        RemoteViews views = buildRemoteViews(context,postcode);
+        AppWidgetManager manager = AppWidgetManager.getInstance(context);
+        ComponentName thisWidget = new ComponentName(context, PostcodeAppWidgetProvider.class);
+        manager.updateAppWidget(thisWidget, views);
+    }
 
-        @Override
-        public int onStartCommand(Intent intent, int flags, int startId) {
-            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-            Criteria criteria = new Criteria();
-            criteria.setCostAllowed(false);
-            criteria.setAccuracy(Criteria.ACCURACY_COARSE);
-            String providerName = locationManager.getBestProvider(criteria, true);
-            Location lastLocation = locationManager.getLastKnownLocation(providerName);
-            onLocationChanged(lastLocation);
-            Log.d("PostcodeAppWidgetProvider","onStartCommand:"+providerName);
-            locationManager.requestLocationUpdates(providerName, FIVE_MINS_MS, FIVE_HUNDRED_M, UpdateService.this);
-            return START_STICKY;
-        }
+    @Override
+    public void updatedLocation(final Location l) {
+        lastLocation = l;
+    }
 
+    @Override
+    public void locationFindFail() {
+    }
 
+    @Override
+    public void postcodeLookupFail() {
+    }
 
-        @Override
-        public void onDestroy() {
-            Log.d("PostcodeAppWidgetProvider","onDestroy");
-            super.onDestroy();
-            LocationManager locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-            locationManager.removeUpdates(this);
-        }
-
-        @Override
-        public IBinder onBind(final Intent intent) {
-            return null;
-        }
-
-
-        @Override
-        public void onLocationChanged(final Location location) {
-            Log.d("PostcodeAppWidgetProvider","onLocationChanged:"+location);
-
-            Runnable runner = new Runnable() {
-                @Override
-                public void run() {
-                    RemoteViews views = buildRemoteViews(location);
-                    AppWidgetManager manager = AppWidgetManager.getInstance(UpdateService.this);
-                    ComponentName thisWidget = new ComponentName(UpdateService.this, PostcodeAppWidgetProvider.class);
-                    manager.updateAppWidget(thisWidget, views);
-                }
-            };
-            Thread tmpThread = new Thread(runner,"Widget Update");
-            tmpThread.setDaemon(true);
-            tmpThread.start();
-
-        }
-
-        @Override
-        public void onStatusChanged(String provider, int status, Bundle extras) {
-            Log.d("PostcodeAppWidgetProvider","onStatusChanged:");
-        }
-
-        @Override
-        public void onProviderEnabled(String provider) {
-            Log.d("PostcodeAppWidgetProvider","onProviderEnabled:"+provider);
-        }
-
-        @Override
-        public void onProviderDisabled(String provider) {
-            Log.d("PostcodeAppWidgetProvider","onProviderDisabled:"+provider);
-        }
-
-        private RemoteViews buildRemoteViews(Location location) {
-            final RemoteViews views = new RemoteViews(getPackageName(), R.layout.appwidget);
-            String postCode;
-            try {
-                postCode = PostcodeBackend.get(location.getLatitude(),location.getLongitude());
-                views.setTextViewText(R.id.postcode, postCode);
-                views.setTextViewText(R.id.accuracy, Float.toString(location.getAccuracy()));
-            } catch (PostcodeException e) {
-                Log.e(TAG, "Exception getting postcode for " + location, e);
-            }
-            return views;
-        }
+    private RemoteViews buildRemoteViews(final Context context,final String postCode) {
+        final RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.appwidget);
+        views.setTextViewText(R.id.postcode, postCode);
+        views.setTextViewText(R.id.accuracy, Integer.toString((int) lastLocation.getAccuracy()));
+        views.setTextViewText(R.id.location, String.format("%2.2f / %2.2f", lastLocation.getLatitude(),lastLocation.getLongitude()));
+        views.setTextViewText(R.id.time, DateFormat.getTimeInstance().format(lastLocation.getTime()));
+        return views;
     }
 }
